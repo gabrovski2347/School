14245b738f4fc95c09134a371649951ce72469cd
gabrovski2347 <48479951+gabrovski2347@users.noreply.github.com>
2021-06-15 09:24:45 +0300

    Add files via upload

---
+++ P06/P06/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace P06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int cols = 3;
+
+            var matrix = new char[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    char symbol = char.Parse(Console.ReadLine());
+                    matrix[i, j] = symbol;
+                }
+            }
+
+            int winner = 0; // 0 - no winner, //1 -x , 2 - O
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols-1; j++)
+                {
+                    if (matrix[i, j] != matrix[i, j+1])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[i, j] == 'X')
+                        {
+                            winner = 1;
+                        }
+                        else
+                            winner = 2;
+                    }
+                }               
+            }
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] != matrix[i + 1, j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[i, j] == 'X')
+                        {
+                            winner = 1;
+                        }
+                        else
+                            winner = 2;
+                    }
+                }
+            }
+
+            if (winner == 0)
+            {
+                Console.WriteLine("There is no winner");
+            }
+            else if (winner == 1)
+            {
+                Console.WriteLine("The winner is: X");
+            }
+            else
+            {
+                Console.WriteLine("The winner is: O");
+            }
+        }
+    }
+}
---
+++ P08/P08/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace P08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var info = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                int cols = info[0];
+                int rows = info[1];
+                int[,] matrix = new int[rows, cols];
+                int min = int.MaxValue;
+                int max = int.MinValue;
+                double avg = 0;
+                for (int j = 0; j < rows; j++)
+                {
+                    var input = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                    for (int a = 0; a < input.Length; a++)
+                    {
+                        matrix[j, a] = input[a];
+                        if (max < input[a])
+                        {
+                            max = input[a];
+                        }
+                        if (min > input[a])
+                        {
+                            min = input[a];
+                        }
+                        avg += input[a];
+                    }
+                }
+                avg /= rows + cols;
+                Console.WriteLine($"{min} {max} {avg}");
+            }
+        }
+    }
+}
